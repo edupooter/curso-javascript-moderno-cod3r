ff0e103247d261a4282b4e43936325fb4e366203
Eduardo Pooter Reis <edupooterreis@gmail.com>
2024-01-04 14:34:04 -0300

    Coded a JSON parser example with map.

---
+++ array/map2.js
@@ -0,0 +1,14 @@
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.9}',
+    '{"nome": "Kit de Lápis", "preco": 41.22}',
+    '{"nome": "Caderno", "preco": 7.5}',
+]
+
+// Retornar array com apenas preços
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+
+console.log(resultado)
