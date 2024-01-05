8b679b761e55b0c484c5e0a57bf7455e559764cf
Eduardo Pooter Reis <edupooterreis@gmail.com>
2024-01-05 16:09:03 -0300

    Added concat examples.

---
+++ array/concat.js
@@ -0,0 +1,8 @@
+const filhas = ['Almira', 'Cibalena']
+const filhos = ['Almo', 'Uesclei']
+
+const todos = filhas.concat(filhos, 'Fulaninho')
+
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
