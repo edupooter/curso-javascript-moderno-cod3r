24ef0e5c0a696a1bde6977b7d72661057c1c263d
Eduardo Pooter Reis <edupooterreis@gmail.com>
2024-01-31 16:01:59 -0300

    Created a memory database and endpoints to create and get products.

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -1,15 +1,27 @@
 const porta = 3003
 
+const bancoDados = require('./bancoDados')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('Middleware')
-    next()
-})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
-    res.status(200).json({nome: 'Notebook', preco: 1234.5})
+    res.send(bancoDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDados.getProduto(req.params.id))
+})
+
+app.post('/produtos/', (req, res, next) => {
+    const produto = bancoDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
